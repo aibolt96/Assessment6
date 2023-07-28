79e0e04b6c392e6be73192187fddebeaafa63b1c
Austin Bolton <aibolt3@gmail.com>
2023-07-28 15:04:07 -0500

    part 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const playerRecord = {
 const app = express();
 
 app.use(express.json());
-app.us(express.static(`public`))
+app.use(express.static(`public`));
 
 // Add up the total health of all the robots
 const calculateTotalHealth = (robots) =>
