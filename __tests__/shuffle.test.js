807155aa67e203c2b9c9c89628e87afb9a5f2abd
Austin Bolton <aibolt3@gmail.com>
2023-07-28 15:31:04 -0500

    part 3

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,14 @@
 const shuffle = require("../src/shuffle");
 
-describe("shuffle should...", () => {
-  // CODE HERE
+const testArray = [50, 77, 512, 99, 4]
+
+describe("tests for shuffle", () => {
+  test('returns an array of the same length' , () => {
+    expect(shuffle(testArray).length).toBe(testArray.length)
+  })
+
+  test('items in array have been shuffled' , () => {
+    const shuffledArray = shuffle(testArray)
+    expect(shuffledArray).not.toBe(testArray)
+  })
 });
