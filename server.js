645017c6a130495e5cdaef78442c6e66a869f5ab
Austin Bolton <aibolt3@gmail.com>
2023-07-29 10:01:30 -0500

    part 5

--- server.js
+++ server.js
@@ -2,16 +2,6 @@ const express = require("express");
 const bots = require("./src/botsData");
 const shuffle = require("./src/shuffle");
 
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: 'aad7474e7e4c4104a57793c81b50b95d',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
-
-// record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
-
 const playerRecord = {
   wins: 0,
   losses: 0,
@@ -21,6 +11,15 @@ const app = express();
 app.use(express.json());
 app.use(express.static(`public`));
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'aad7474e7e4c4104a57793c81b50b95d',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 // Add up the total health of all the robots
 const calculateTotalHealth = (robots) =>
   robots.reduce((total, { health }) => total + health, 0);
@@ -48,9 +47,11 @@ const calculateHealthAfterAttack = ({ playerDuo, compDuo }) => {
 
 app.get("/api/robots", (req, res) => {
   try {
+    rollbar.info('All robots requested')
     res.status(200).send(botsArr);
   } catch (error) {
     console.error("ERROR GETTING BOTS", error);
+    rollbar.error('Failed to get all Robots')
     res.sendStatus(400);
   }
 });
@@ -58,9 +59,11 @@ app.get("/api/robots", (req, res) => {
 app.get("/api/robots/shuffled", (req, res) => {
   try {
     let shuffled = shuffle(bots);
+    rollbar.warning('Choice of robots was requested.')
     res.status(200).send(shuffled);
   } catch (error) {
     console.error("ERROR GETTING SHUFFLED BOTS", error);
+    rollbar.error('Failed to retrieve choice of robots')
     res.sendStatus(400);
   }
 });
@@ -77,9 +80,11 @@ app.post("/api/duel", (req, res) => {
     // comparing the total health to determine a winner
     if (compHealth > playerHealth) {
       playerRecord.losses += 1;
+      rollbar.info('Player defeated')
       res.status(200).send("You lost!");
     } else {
       playerRecord.losses += 1;
+      rollbar.info('Player victorious')
       res.status(200).send("You won!");
     }
   } catch (error) {
@@ -91,6 +96,7 @@ app.post("/api/duel", (req, res) => {
 app.get("/api/player", (req, res) => {
   try {
     res.status(200).send(playerRecord);
+    rollbar.warning('Player Score updated')
   } catch (error) {
     console.log("ERROR GETTING PLAYER STATS", error);
     res.sendStatus(400);
